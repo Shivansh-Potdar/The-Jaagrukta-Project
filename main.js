1e46d032af1c0ba42057fb7e87d4e0b7a1656e28
Shivansh Potdar <loldude482@gmail.com>
2022-07-06 14:26:28 +0530

    added onhover event for boxes

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function open_link(link){
     window.open(link, "_blank").focus();
 }
+
+function ShowText(x){
+    document.getElementById(x).style.display = "block";
+}
+
+function HideText(x){
+    document.getElementById(x).style.display = "none";
+}
